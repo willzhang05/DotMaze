0e012dd5e3514f65b8988a4959b5333e98be3c08
William Zhang <willzhang05@gmail.com>
2015-08-06 23:05:26 -0400

    ignore copies of files

--- scripts/game - Copy.js
+++ scripts/game - Copy.js
@@ -11,10 +11,37 @@ window.onkeydown = function(e){
 	animate(e);
 }
 
+function handleKeyDown(event)
+    {
+        if (event.keyCode == 65) 
+                key_left = true;
+        else if (event.keyCode == 68)
+            key_right = true;
+    }
+
+function handleKeyUp(event){
+   if (event.keyCode == 65)  [
+		key_left = false;
+	} else if (event.keyCode == 68){
+	   key_right = false;
+   } 
+}
+
+window.addEventListener('keydown', handleKeyDown, true)
+window.addEventListener('keyup', handleKeyUp, true)
+
+var maxspeed = 6;
+var xforce = 0;
+var pixelx = 0;
+var direction = 1;
+var key_left = false;
+var key_right = false;
+
 var w = window.innerWidth;
 var h = window.innerHeight;
 var xpos = 125;
 var ypos = 125;
+var dir = 0;
 
 window.onresize = function(e){
 	var w = window.innerWidth;
@@ -37,18 +64,34 @@ dot.anchor.y = 0.5;
 dot.position.x = xpos;
 dot.position.y = ypos;
 stage.addChild(dot);
-// start animating
+
 function animate(e) {
     requestAnimationFrame(animate);
     if(e.keyCode == 65){
-		dot.position.x -= 10;
+		xforce--;
+		direction = -1;
 	} else if (e.keyCode == 68){
+		xforce++;
+		direction = 1;
 		dot.position.x += 10;
+		if (xforce > maxspeed){
+			xforce = maxspeed;
+		} else if (xforce < -maxspeed){
+			xforce = -maxspeed;
+		}
+        
+	} else if (e.keyCode != 65 && e.keyCode !=68){
+		pixelx = 0;
+		xforce = 0;
 	} else if (e.keyCode == 83){
 		dot.position.y += 10;
 	} else if (e.keyCode == 87){
 		dot.position.y -= 10;
+	} else {
+		pixelx + xforce;
 	}
+	xpos = xpos + pixelx;
+	dir = direction;
     renderer.render(stage);
 }
 
